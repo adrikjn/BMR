069ddb4c8b5684c6091badc54569fe317142e370
Adrien Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-03-01 15:14:08 +0100

    Ajout des required et Traitement de la page contact

--- lib/traitement.php
+++ lib/traitement.php
@@ -1,5 +1,43 @@
 <?php 
 
+// Vérifier si le formulaire a été soumis
+if($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+    // Vérifier si le champ "name" n'est pas vide
+    if(empty($_POST["name"])){
+        echo "Le champ 'Prénom & NOM' est obligatoire.";
+    } else {
+        // Limiter la longueur du champ "name" à 50 caractères maximum
+        $name = substr($_POST['name'], 0, 50);
+
+        // Vérifier su le champ "email" n'est pas vide
+        if(empty($_POST['email'])) {
+            echo "Le champ 'Adresse email' est obligatoire";
+        } else {
+
+            // Vérifier si l'email est valide
+            if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                echo "L'adresse email n'est pas valide";
+            } else {
+
+                // Vérifier si le champ "besoin" a été sélectionné
+                if($_POST['besoin'] == "Clique pour ouvrir le menu déroulant") {
+                    echo "Veuillez sélectionner votre besoin.";
+                } else {
+
+                    // Vérifier si le champ "message" n'est pas vide
+                    if(empty($_POST['message'])) {
+                        echo "Le champ 'Un message à nous parvenir avant la prise de contacte ?' est obligatoire.";
+                    } else {
+
+                        // Si toutes les conditions sont remplies, afficher un message de succès
+                        echo "Le formulaire a été soumis avec succès";
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 // <!-- name / email / tel / besoin / message-->
--- views/contact.php
+++ views/contact.php
@@ -3,27 +3,27 @@
         <form action="lib/traitement.php" method="POST">
             
             <div class="form-floating mt-3">
-                <input type="text" class="form-control bg-light" id="name" placeholder="Prénom NOM" name="name">
+                <input type="text" class="form-control bg-light" id="name" placeholder="Prénom NOM" name="name" required>
                 <label class="fw-bold" for="name" style=>Prénom & NOM :</label>
               </div>
             
             <div class="row mt-5 mb-5">
                 <div class="col-6">
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control bg-light" id="email" placeholder="nom@exemple.com" name="email">
+                        <input type="email" class="form-control bg-light" id="email" placeholder="nom@exemple.com" name="email" required>
                         <label class="fw-bold"  for="email">Adresse email :</label>
                       </div>
                 </div>
                 <div class="col-6">
                     <div class="form-floating mb-3">
-                        <input type="tel" name="tel" id="tel" placeholder="+33 X-XX-XX-XX-XX" class="form-control bg-light" name="tel">
+                        <input type="tel" name="tel" id="tel" placeholder="+33 X-XX-XX-XX-XX" class="form-control bg-light" name="tel" required>
                         <label class="fw-bold"  for="tel">Téléphone :</label>
                     </div>
                 </div>
             </div>
 
             <div class="form-floating mt-5 mb-5">
-                <select class="form-select bg-light" id="besoin" aria-label="Floating label select example" name="besoin">
+                <select class="form-select bg-light" id="besoin" aria-label="Floating label select example" name="besoin" required>
                   <option selected disabled>Clique pour ouvrir le menu déroulant</option>
                   <option value="tissus">Tissus</option>
                   <option value="ameublement">Tissus pour l'Ameublement</option>
@@ -36,7 +36,7 @@
               </div>
             
               <div class="form-floating mb-5 mt-5">
-                <textarea class="form-control bg-light" placeholder="Leave a comment here" id="message" style="height: 100px" name="message"></textarea>
+                <textarea class="form-control bg-light" placeholder="Leave a comment here" id="message" style="height: 100px" name="message" required></textarea>
                 <label class="fw-bold"  for="message">Un message à nous parvenir avant la prise de contacte ?</label>
               </div>
 
